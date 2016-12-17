cb30d8e335b938e8611746cd92c170a88adf9e28
jdwang <383287471@qq.com>
2016-12-17 21:11:48 +0800

    add README.md and spark sklearn

---
+++ README.md
@@ -0,0 +1,6 @@
+# test Pyspark in Pycharm
+ 
+- script1 : test.py
+    - just simple test whether Pyspark can run on the Pycharm 
+- script2 : spark_sklearn_test.py
+    - test third-part package -- [spark sklearn](https://github.com/databricks/spark-sklearn), and simple do a svm test.  
--- spark_sklearn_test.py
+++ spark_sklearn_test.py
@@ -1,45 +1,43 @@
 # encoding=utf8
 """
     Author:  'jdwang'
-    Date:    'create date: 2016-08-14'
+    Date:    'create date: 2016-12-17'; 'last updated date: 2016-12-17'
     Email:   '383287471@qq.com'
     Describe:
 """
-
-import os
+from __future__ import print_function
 import sys
+import os
 
 # 添加 环境变量 SPARK的 跟目录
 SPARK_HOME = '/home/jdwang/spark-2.0.0-bin-hadoop2.7/'
 os.environ['SPARK_HOME'] = SPARK_HOME
 
-# Append python spark lib (pyspark)  to Python Path
-# 添加两个库
 sys.path.extend([
     SPARK_HOME + 'python/lib/pyspark.zip',
     SPARK_HOME + 'python/lib/py4j-0.10.1-src.zip']
 )
 
-try:
-    from pyspark import SparkContext
-    from pyspark import SparkConf
+from pyspark import SparkContext
+from pyspark import SparkConf
 
-    print ("Successfully imported Spark Modules")
-
-except ImportError as e:
-    print ("Can not import Spark Modules", e)
-    sys.exit(1)
 
 if __name__ == '__main__':
     conf = SparkConf()
-    # conf.setMaster("local[3]")
+    conf.setMaster("local[3]")
     # 指定具体的Master机器 地址和端口
-    conf.setMaster("spark://jdwang-HP:7077")
+    # conf.setMaster("spark://jdwang-HP:7077")
     conf.setAppName("spark_test")
     # 可以设置属性等
     # conf.set("spark.executor.memory", "12g")
     sc = SparkContext(conf=conf)
     # 测试
-    data = sc.textFile('UserPurchaseHistory.csv')
-    print(data.map(lambda x: x.split(',')).collect())
+    from sklearn import svm, datasets
+    from spark_sklearn import GridSearchCV
 
+    iris = datasets.load_iris()
+    parameters = {'kernel': ('linear', 'rbf'), 'C': [1, 10]}
+    svr = svm.SVC()
+    clf = GridSearchCV(sc, svr, parameters)
+    clf.fit(iris.data, iris.target)
+    print(clf.predict(iris.data))
